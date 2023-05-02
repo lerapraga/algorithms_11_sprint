403e1275ae7bf2ae6bc92c4e33de8a59ed92d657
Lera Praga <49233390+lerapraga@users.noreply.github.com>
2023-05-02 15:05:24 +0300

    Update typing_trainer.py after 1 review

--- typing_trainer.py
+++ typing_trainer.py
@@ -1,5 +1,6 @@
-# 86738910
+# 86829674
 
+from collections import Counter
 from typing import Tuple
 
 NUMBER_OF_ROWS = 4
@@ -7,31 +8,22 @@ NUMBER_OF_PLAYERS = 2
 
 
 def typing_trainer(k: int, digits: str) -> int:
-    nums_dict: dict = {}
-    for num in digits:
-        nums_dict[num] = nums_dict.get(num, 0) + 1
-    result: int = 0
-    for num in nums_dict.values():
-        if num <= k * NUMBER_OF_PLAYERS:
-            result += 1
-    return result
+    cnt = Counter(digits)
+    new_cnt = [0 if value > k * NUMBER_OF_PLAYERS else 1 for value in cnt.values()]
+    return sum(new_cnt)
 
 
 def read_input() -> Tuple[int, str]:
-    digits: str = ''
-    k: int = int(input())
-    for _ in range(NUMBER_OF_ROWS):
-        for c in input():
-            if c.isdigit():
-                digits += c
-    return k, digits
-
-
-def main() -> None:
-    k, digits = read_input()
-    result = typing_trainer(k, digits)
-    print(result)
+    digits = []
+    k = int(input())
+    for i in range(4):
+        digits.append(input())
+    digits = "".join(digits)
+    digits = "".join(c for c in digits if c.isdecimal())
+    return (k, digits)
 
 
 if __name__ == '__main__':
-    main()
+    k, digits = read_input()
+    result = typing_trainer(k, digits)
+    print(result)
